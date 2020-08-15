dfa43af2a870c2f2ef523ac1b10e7f9a73b95cb3
Russ <3870824+pocketfulofrage@users.noreply.github.com>
2020-08-15 14:58:35 -0400

    Update config.h

--- config.h
+++ config.h
@@ -25,6 +25,12 @@ static char *colors[][3] = {
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+};
+
 typedef struct {
 	const char *name;
 	const void *cmd;
