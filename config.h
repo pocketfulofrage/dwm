87c0d32ffdd53be2b62d2e2880e0b264cedd499d
Russ <3870824+pocketfulofrage@users.noreply.github.com>
2020-08-15 16:35:03 -0400

    Update config.h

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#770000";
 static char selbgcolor[]            = "#005577";
-static const unsigned int baralpha  = 0xd0;
+static const unsigned int baralpha  = 0xb4;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3] = {
        /*               fg           bg           border   */
